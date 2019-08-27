b138e350cbf33d5e343a18c22a1050869b745127
Sérgio Ricardo <sergio.ricardo@dce.ufpb.br>
2019-08-27 20:36:05 -0300

    Update Browser.java

--- src/test/java/br/com/jl/taskit/constants/Browser.java
+++ src/test/java/br/com/jl/taskit/constants/Browser.java
@@ -2,8 +2,8 @@ package br.com.jl.taskit.constants;
 
 public interface Browser {
 
-    static final String CHROME = "chrome";
-    static final String FIREFOX = "firefox";
-    static final String REMOTE_CHROME = "cremote";
+    String CHROME = "chrome";
+    String FIREFOX = "firefox";
+    String REMOTE_CHROME = "cremote";
 
 }
